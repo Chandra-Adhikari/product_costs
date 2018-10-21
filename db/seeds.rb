ca239223f810c89cf6f1fb4468de63d345ba0c7b
Chandra Adhikari <chandra.adhikari1994@gmail.com>
2018-10-21 20:45:28 +0530

    Manage product cost as per location

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..50).each do |x|
+	Product.create(name: "product"+x.to_s, price: 1*x , image: "http://res.cloudinary.com/dhzgmlrs5/image/upload/c_fit,h_300,w_300/v1539211444/nhhbimkrsfur72hhlrtr.jpg")
+end
